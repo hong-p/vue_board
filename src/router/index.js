b49e8abc0454c6ecdd494b1b096173378d5caccc
inyong hong <iyhong0721@gmail.com>
2021-09-05 14:46:57 +0900

    build: 주제별 내용 조회 경로 변경2

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ const routes = [
     component: Home
   },
   {
-    path: '/board',
+    path: '/vue_board/board',
     name: 'Board',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
